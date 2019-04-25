d447375fe4d367eee8ae04653f4b231a2d98d1f8
TIAGO.BLIMA <tiago.blima@ufrpe.br>
2019-04-25 15:54:09 -0300

    busca em largura e busca em profundidade

--- bfs_search.py
+++ bfs_search.py
@@ -1,26 +1,6 @@
 from collections import defaultdict
 
 
-class Graph():
-    def __init__(self, start, end):
-        self.edges = defaultdict(list)
-
-    def add_edge(self, source, destination, cost):
-        self.edges[source].append((destination, cost))
-
-
-g = Graph(start='S', end='G')
-
-g.add_edge('S', 'A', 2)
-g.add_edge('S', 'B', 1)
-g.add_edge('S', 'G', 9)
-g.add_edge('A', 'C', 2)
-g.add_edge('A', 'D', 3)
-g.add_edge('B', 'D', 2)
-g.add_edge('B', 'E', 4)
-g.add_edge('C', 'G', 4)
-g.add_edge('D', 'G', 4)
-
 def bfs(g, start='S', end='G'):
     visited = []
     queue = [start]
